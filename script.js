b9689582baae9273d6b42b12f9ff38fd57154e5c
Felipe-Krainer <fdetoled@purdue.edu>
2024-10-27 16:38:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function populateHeadings() {
+
+    $('#name').text("Felipe");       
+    $('#major').text("Computer Information Technology"); 
+    $('#year').text("Sophomore");       
+
+    $('#location').html("<em>Brazil</em>");
+}
+
+$(document).ready(populateHeadings);
+
